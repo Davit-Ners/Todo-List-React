11ac31c30dfc89e8763fa6f5761f3f7624e436f1
Davit <davit.ners02@gmail.com>
2025-03-14 15:29:14 +0100

    Add css to tasks + different if is done

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,17 +1,20 @@
-export default function Task({ id, name, desc = '', priority, onDelete = () => {} }) {
+import style from './task.module.css';
+import clsx from 'clsx';
+
+export default function Task({ id, name, desc = '', priority, done, onDelete = () => {} }) {
 
     const handleDelete = () => {
         onDelete(id)      
     };
     
     return (
-        <div className="task">
+        <div className={clsx(style['task'], done && style['is-done'])}>
             <div className="txt">
                 <p>{name}</p>
                 <p>{desc}</p>
             </div>
-            <div className="task-btn">
-                <button>Terminer</button>
+            <div className={style['task-btn']}>
+                <button disabled={done}>Terminer</button>
                 <button onClick={handleDelete}>Supprimer</button>
             </div>
         </div>
