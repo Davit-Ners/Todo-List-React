ad3dd366245c79f1121fae0866c4079102496530
Davit <davit.ners02@gmail.com>
2025-03-14 15:12:20 +0100

    Add use State in todo-list for data + setnewtask function

--- src/containers/todo-list/todo-list.jsx
+++ src/containers/todo-list/todo-list.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Task from "../../components/task/task.jsx";
 import TodoForm from "../../components/todo-form/todo-form.jsx";
-import data from '../../data/todo.json'
+import json from '../../data/todo.json'
 import style from './todo-list.module.css';
 
 export default function TodoList() {
 
+    const [ data, setData ] = useState(json.todo);
+    const [ taskId, setTaskId ] = useState(json.lastId);
+
     const onSubmit = (name, desc, priority) => {
-        console.log(name, desc, priority);
+        setTaskId(taskId + 1);
+        setData(val => [...val, { id: taskId + 1, name, desc, priority }]);
     }
     
     return (
@@ -15,7 +20,7 @@ export default function TodoList() {
 
             <div>
                 <h2>Liste des taches</h2>
-                {data.todo.map(task => <Task key={task.id} {...task} />)}
+                {data.map(task => <Task key={task.id} {...task} />)}
             </div>
         </div>
     );
