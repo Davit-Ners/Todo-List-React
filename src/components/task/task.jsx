e6bbbf9ef0e9ec09958e9fce1de0691164d1997e
Davit <davit.ners02@gmail.com>
2025-03-17 11:05:11 +0100

    Add limit date + display formatted

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,7 +1,13 @@
 import style from './task.module.css';
 import clsx from 'clsx';
 
-export default function Task({ id, name, desc = '', priority, done, onDelete = () => {}, onFinish = () => {}, onModify = () => {}, onReset = () => {} }) {
+export default function Task({ id, name, desc = '', priority, done, failed = false, createdAt, dateLimit,  
+    onDelete = () => {}, onFinish = () => {}, onModify = () => {}, onReset = () => {} 
+}) {
+
+    const formateCreateDate = new Date(parseInt(createdAt));
+    const formatLimitDate = dateLimit && new Date(parseInt(dateLimit));
+    console.log(dateLimit);
 
     const handleDelete = () => {
         onDelete(id)      
@@ -20,10 +26,11 @@ export default function Task({ id, name, desc = '', priority, done, onDelete = (
     }
     
     return (
-        <div className={clsx(style['task'], done && style['is-done'])}>
+        <div className={clsx(style['task'], done && style['is-done'], failed && style['failed'])}>
             <div className="txt">
                 <p>{name}{priority === 'high' ? <span className={style['urgent']}>(Urgent)</span> : ''}</p>
                 <p>{desc}</p>
+                {dateLimit && <p>A faire pour le {formatLimitDate.toLocaleDateString()} <br /> à {formatLimitDate.toLocaleTimeString().slice(0, 5)}</p>}
             </div>
             <div className={style['task-btn']}>
                 <button onClick={handleDone} disabled={done}>Terminer</button>
